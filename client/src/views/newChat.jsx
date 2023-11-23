0b10e22f9d5d03d75815ce864eec6b57d1b719e5
shellinw <shellin.winata@yahoo.co.id>
2023-11-23 12:32:48 +0700

    fix:uninstallCO

--- client/src/views/newChat.jsx
+++ client/src/views/newChat.jsx
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-import io from "socket.io-client";
+// import io from "socket.io-client";
 
-let socket;
+// let socket;
 
-const Chat = () => {
-    const [name, setName] = useState("");
+// const Chat = () => {
+//     const [name, setName] = useState("");
 
-    const URL = "http://localhost:3000";
-    // const [searchParams] = useSearchParams();
+//     const URL = "http://localhost:3000";
+//     // const [searchParams] = useSearchParams();
 
-    useEffect(() => {
-        socket = io(URL);
-        return socket.emit("disconnect");
-        socket.off();
-    }, []);
-    return (
-        <>
-            <div className="joinOuterContainer">
-                <div className="joinInnerContainer">
-                    <h1 className="heading">Join</h1>
-                    <div>
-                        <input
-                            placeholder="Name"
-                            className="joinInput"
-                            type="text"
-                            onChange={(event) => setName(event.target.value)}
-                        />
-                    </div>
-                    <button
-                        onClick={() => {
-                            socket.emit("join", { name }, () => {});
-                        }}
-                        className={"button mt-20"}
-                        type="submit"
-                    >
-                        CHAT WITH THIS USER
-                    </button>
-                </div>
-            </div>
-        </>
-    );
-};
+//     useEffect(() => {
+//         socket = io(URL);
+//         return socket.emit("disconnect");
+//         socket.off();
+//     }, []);
+//     return (
+//         <>
+//             <div className="joinOuterContainer">
+//                 <div className="joinInnerContainer">
+//                     <h1 className="heading">Join</h1>
+//                     <div>
+//                         <input
+//                             placeholder="Name"
+//                             className="joinInput"
+//                             type="text"
+//                             onChange={(event) => setName(event.target.value)}
+//                         />
+//                     </div>
+//                     <button
+//                         onClick={() => {
+//                             socket.emit("join", { name }, () => {});
+//                         }}
+//                         className={"button mt-20"}
+//                         type="submit"
+//                     >
+//                         CHAT WITH THIS USER
+//                     </button>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
 
-export default Chat;
+// export default Chat;
--- client/src/views/newViews.jsx
+++ client/src/views/newViews.jsx
@@ -1,16 +1,16 @@
-import axios from "axios";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
-const NewViews = () => {
-    const access = localStorage.getItem("access_token");
-    const navigate = useNavigate();
-    useEffect(() => {
-        const fetchClubs = async () => {
-            setClubs(response?.data);
-        };
-    }, []);
-    return <></>;
-};
+// const NewViews = () => {
+//     const access = localStorage.getItem("access_token");
+//     const navigate = useNavigate();
+//     useEffect(() => {
+//         const fetchClubs = async () => {
+//             setClubs(response?.data);
+//         };
+//     }, []);
+//     return <></>;
+// };
 
-export default NewViews;
+// export default NewViews;
